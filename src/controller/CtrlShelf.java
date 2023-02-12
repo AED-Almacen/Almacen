d0b97aadcfd10380db0e161a9b3913bfc5d3e28d
jvarc <jcvargas273@gmail.com>
2023-02-12 23:48:52 +0000

    entity shelf

--- src/controller/CtrlShelf.java
+++ src/controller/CtrlShelf.java
@@ -1,32 +1,127 @@
 package controller;
 
 import model.ShelfQueries;
+import model.Warehouse;
+import model.WarehouseQueries;
 import view.Shelf;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class CtrlShelf implements ActionListener {
     private Shelf shelf;
     private ShelfQueries queries;
 
+    private Warehouse warehouse;
+    private WarehouseQueries queriesWarehouse;
+
+
+
     private void windowConfig() {
-        this.shelf.setTitle("Estanterías");
+        this.shelf.setTitle("Shelf");
         this.shelf.setLocationRelativeTo(null);
-        this.shelf.setSize(400, 400);
+        this.shelf.setSize(600, 600);
         this.shelf.setVisible(true);
     }
 
+    private void readShelf() {
+        this.shelf.getTextArea1().setText("");
+
+        ArrayList<model.Shelf> shelves = queries.readShelves();
+        ArrayList<model.Warehouse> warehouses = queriesWarehouse.readWarehouses();
+
+        if(shelves == null) {
+            this.shelf.getTextArea1().append("No hay estanterias en la base de datos.");
+        }else{
+            for (model.Shelf shelf : shelves) {
+                this.shelf.getTextArea1().append(shelves.toString()+"\n");
+            }
+        }
+
+        if(warehouses == null) {
+            this.shelf.getTextArea1().append("No hay almacenes en la base de datos.");
+        }else{
+            for (model.Warehouse warehouse : warehouses) {
+                this.shelf.getWarehouseCombo().addItem(warehouse.getId());
+            }
+        }
+    }
     public CtrlShelf() {
-        this.shelf = new Shelf();
+        this.shelf = new view.Shelf();
         windowConfig();
 
         this.queries = new ShelfQueries();
-        this.shelf.getShelfButton().addActionListener(this);
+        this.queriesWarehouse = new WarehouseQueries();
+        this.shelf.getAddBtn().addActionListener(this);
+        this.shelf.getDropBtn().addActionListener(this);
+        this.shelf.getUpdateBtn().addActionListener(this);
+        readShelf();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("estantería");
+        if (e.getSource() == this.shelf.getAddBtn()) {
+            try {
+                int idWarehouse;
+                String codEstanteria = shelf.getCodShelfText().getText();
+                int warehouseCombo = shelf.getWarehouseCombo().getSelectedIndex();
+                System.out.println(warehouseCombo);
+
+                idWarehouse = queriesWarehouse.readWarehouse(warehouseCombo).getId();
+                System.out.println("idWarehouse" + idWarehouse);
+
+                if (codEstanteria.equals("") || warehouseCombo == 0) {
+                    JOptionPane.showMessageDialog(null,
+                            "Error al añadir estanteria. Debe rellenar todos los campos.");
+                } else {
+                    this.queries.createShelf( codEstanteria, idWarehouse);
+                    readShelf();
+                }
+            } catch (Exception exception) {
+                System.out.println(exception);
+                JOptionPane.showMessageDialog(null,
+                        "Error al añadir estanteria. el almacen debe tener descripcion y direccion.");
+            }
+        } else if (e.getSource() == this.shelf.getDropBtn()) {
+            try {
+                if(this.queries.readShelf(Integer.parseInt(shelf.getIdText().getText())) == null) {
+                    JOptionPane.showMessageDialog(null,
+                            "Error al borrar estanteria. El almacen no existe.");
+                    return;
+                };
+                this.queries.deleteShelf(Integer.parseInt(shelf.getIdText().getText()));
+                readShelf();
+            } catch (Exception exception) {
+                JOptionPane.showMessageDialog(null,
+                        "Error al borrar estanteria. Debes especificar el id de del estanteria a borrar.");
+            }
+        } else if (e.getSource() == this.shelf.getUpdateBtn()) {
+
+            try {
+                if(this.queries.readShelf(Integer.parseInt(shelf.getIdText().getText())) == null) {
+                    JOptionPane.showMessageDialog(null,
+                            "Error al actualizar estanteria. El estanteria no existe.");
+                    return;
+                };
+                int id = Integer.parseInt(shelf.getIdText().getText());
+                int idWarehouse;
+                String codEstanteria = shelf.getCodShelfText().getText();
+                int warehouseCombo = shelf.getWarehouseCombo().getSelectedIndex();
+                idWarehouse = queriesWarehouse.readWarehouse(warehouseCombo).getId();
+
+                if (codEstanteria.equals("") || warehouseCombo == 0) {
+                    JOptionPane.showMessageDialog(null,
+                            "Error al añadir estanteria. Debe rellenar todos los campos.");
+                } else {
+                    this.queries.updateShelf(id, codEstanteria, idWarehouse);
+                    readShelf();
+                }
+            } catch (Exception exception) {
+                JOptionPane.showMessageDialog(null,
+                        "Error al actualizar el almacen. Debes especificar el id del almacen a actualizar.");
+            }
+        }
     }
 }
--- src/view/Shelf.java
+++ src/view/Shelf.java
@@ -4,18 +4,50 @@ import javax.swing.*;
 
 public class Shelf extends JFrame{
     private JPanel window;
-    private JButton shelfButton;
+    private JButton addBtn;
+    private JButton dropBtn;
+    private JButton updateBtn;
+    private JTextArea textArea1;
+    private JComboBox warehouseCombo;
+    private JTextField codShelfText;
+
+
+
+    private JTextField idText;
+
+
+    public void setTextArea1(JTextArea textArea1) {
+        this.textArea1 = textArea1;
+    }
+
+
 
     public Shelf() {
         super();
+        this.window.setSize(1000, 1000);
         setContentPane(window);
     }
 
     public JPanel getWindow() {
-        return window;
+        return this.window;
     }
 
-    public JButton getShelfButton() {
-        return shelfButton;
+    public JButton getAddBtn() {return addBtn;}
+    public JButton getDropBtn() {return dropBtn;}
+    public JButton getUpdateBtn() {return updateBtn;}
+
+    public JTextArea getTextArea1() {
+        return textArea1;
+    }
+
+    public JTextField getIdText() {
+        return idText;
+    }
+
+    public JComboBox getWarehouseCombo() {return warehouseCombo;}
+
+    public JTextField getCodShelfText() {return codShelfText;}
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
