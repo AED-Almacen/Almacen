6dc096e6ef5f99c61062025b71da42a49077cab2
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-02-28 20:44:38 +0000

    new View or JFrame

---
+++ src/view/SubPiece.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+
+public class SubPiece extends JFrame {
+    private JPanel window;
+    private JScrollPane ScrollPane;
+    private JTable table;
+
+    public SubPiece() {
+        super();
+        setContentPane(this.window);
+    }
+}
