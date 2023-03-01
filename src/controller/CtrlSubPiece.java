71503d98c81a024bbfe04d08b84cb6c301765333
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-01 19:32:19 +0000

    refactor SubPiece controller

--- src/controller/CtrlSubPiece.java
+++ src/controller/CtrlSubPiece.java
@@ -3,6 +3,9 @@ package controller;
 import model.PieceQueries;
 import view.SubPiece;
 
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
 public class CtrlSubPiece {
     private final SubPiece subPiece;
     private final PieceQueries queries;
@@ -14,10 +17,14 @@ public class CtrlSubPiece {
         this.subPiece.setVisible(true);
     }
 
-    public CtrlSubPiece() {
+    public CtrlSubPiece(int idPiece) {
         this.subPiece = new SubPiece();
         this.windowConfig();
 
         this.queries = new PieceQueries();
+
+        String title = "Piezas compuestas - Pieza " + this.queries.readPiece(idPiece).getCodPiece();
+        TitledBorder border = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), title);
+        this.subPiece.getScrollPane().setBorder(border);
     }
 }
